2e577a82da5ee99ceddc322da80b7b7e2c3afd63
Dani <daniel.salgado.p@gmail.com>
2015-05-29 12:41:01 +0200

    cleaning globals

--- html/cgi-bin/libs/globals.pm
+++ html/cgi-bin/libs/globals.pm
@@ -1,22 +1,8 @@
 #!/usr/bin/perl
-use warnings;
-use strict;
-
-
-# place for global variables
-
-# my $nombre_app 			= "perlmvc";  # TODO bring this from globals
-
-our $session;  # creo que no se usa
-our %usersJson; # memory representation of users.json
-our %gruposJson; # memory representation of grupos.json
-
-
-our %user; 
-
 package Globals;
 use warnings;
 use strict;
+# place for global variables
 our $q = CGI->new;
 our $server_ip = "http://10.0.191.102"; # in some machines you can use myIp(s)
 our $titulo_web = "PERL MVC"; # web <title>, header <h1> and login header
