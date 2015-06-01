35b84f8dc4f8e70e61275156caaec79efe476514
Dani <daniel.salgado.p@gmail.com>
2015-06-01 10:24:06 +0200

    boot.pm

--- html/cgi-bin/perlmvc/boot.pm
+++ html/cgi-bin/perlmvc/boot.pm
@@ -2,14 +2,13 @@
 use warnings;
 use strict;
 
+# Check for correct configuration for app
+
+# for example:
 # must be readable
 # users.json grupos.json
-
 # must be writable
 # $Paths::sessiones
-
-
 # must be readable and writable
 # $Paths::logs
-
 1;
